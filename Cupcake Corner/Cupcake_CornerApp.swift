5a75ac686a76354bee4ec4243a61695c12e81d23
Takasur Azeem <takasurazeem@gmail.com>
2022-03-26 19:06:54 +0500

    Initial Commit
    With some boiler plate code.

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Takasur Azeem on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
