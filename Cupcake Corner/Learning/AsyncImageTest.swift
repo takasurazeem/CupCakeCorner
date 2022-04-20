c07449e1e3e8e20b0f144525299899faff72fe51
Takasur Azeem <takasurazeem@gmail.com>
2022-04-20 18:25:14 +0500

    Loading an image from a remote server
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ Cupcake Corner/Learning/AsyncImageTest.swift
@@ -0,0 +1,42 @@
+//
+//  AsyncImageTest.swift
+//  Cupcake Corner
+//
+//  Created by Takasur Azeem on 20/04/2022.
+//
+
+import SwiftUI
+
+struct AsyncImageTest: View {
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 200, height: 200)
+            
+            AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } else if phase.error != nil {
+                    Text("There was an error loading the image.")
+                } else {
+                    ProgressView()
+                }
+            }
+            .frame(width: 200, height: 200)
+        }
+    }
+}
+
+struct AsyncImageTest_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncImageTest()
+    }
+}
