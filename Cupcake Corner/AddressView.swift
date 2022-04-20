51ccd605e9a12c8961d3d523114bbedaa545adc1
Takasur Azeem <takasurazeem@gmail.com>
2022-04-20 23:18:38 +0500

    Taking basic order details
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- Cupcake Corner/AddressView.swift
+++ Cupcake Corner/AddressView.swift
@@ -12,21 +12,7 @@ struct AddressView: View {
 
     var body: some View {
         Form {
-            Section {
-                TextField("Name", text: $order.name)
-                TextField("Street address", text: $order.streetAddress)
-                TextField("City", text: $order.city)
-                TextField("Zip", text: $order.zip)
-            }
-
-            Section {
-                NavigationLink {
-                    CheckoutView(order: order)
-                } label: {
-                    Text("Check out")
-                }
-            }
-            .disabled(order.hasValidAddress == false)
+            
         }
         .navigationTitle("Delivery details")
         .navigationBarTitleDisplayMode(.inline)
--- Cupcake Corner/CheckoutView.swift
+++ Cupcake Corner/CheckoutView.swift
@@ -15,26 +15,7 @@ struct CheckoutView: View {
 
     var body: some View {
         ScrollView {
-            VStack {
-                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                } placeholder: {
-                    ProgressView()
-                }
-                .frame(height: 233)
-
-                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
-                    .font(.title)
-
-                Button("Place Order") {
-                    Task {
-                        await placeOrder()
-                    }
-                }
-                .padding()
-            }
+            
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
@@ -44,28 +25,7 @@ struct CheckoutView: View {
             Text(confirmationMessage)
         }
     }
-
-    func placeOrder() async {
-        guard let encoded = try? JSONEncoder().encode(order) else {
-            print("Failed to encode order")
-            return
-        }
-
-        let url = URL(string: "https://reqres.in/api/cupcakes")!
-        var request = URLRequest(url: url)
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST"
-
-        do {
-            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
-
-            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
-            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
-            showingConfirmation = true
-        } catch {
-            print("Checkout failed.")
-        }
-    }
+    
 }
 
 struct CheckoutView_Previews: PreviewProvider {
