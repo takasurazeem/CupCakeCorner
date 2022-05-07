7e529eb87c795679c74147234ab3022925706359
Takasur Azeem <takasurazeem@gmail.com>
2022-05-07 18:15:14 +0500

    Preparing for checkout
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- Cupcake Corner/CheckoutView.swift
+++ Cupcake Corner/CheckoutView.swift
@@ -12,7 +12,26 @@ struct CheckoutView: View {
     @ObservedObject var order: Order
     
     var body: some View {
-        Text("test")
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total is \(order.cost, format: .currency(code: Locale.current.currencyCode ?? "USD"))")
+                    .font(.title)
+                
+                Button("Place Order", action:  {  })
+                    .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
     }
     
 }
