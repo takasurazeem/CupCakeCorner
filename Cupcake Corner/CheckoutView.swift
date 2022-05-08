eeabfb7b7032c4f3e7bb429def1039d9364fb5ed
Takasur Azeem <takasurazeem@gmail.com>
2022-05-08 11:42:12 +0500

    completes challenge 3
    For a more challenging task, see if you can convert our data model from a class to a struct, then create an ObservableObject class wrapper around it that gets passed around. This will result in your class having one @Published property, which is the data struct inside it, and should make supporting Codable on the struct much easier.
    
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- Cupcake Corner/CheckoutView.swift
+++ Cupcake Corner/CheckoutView.swift
@@ -27,7 +27,7 @@ struct CheckoutView: View {
                 }
                 .frame(height: 233)
                 
-                Text("Your total is \(order.cost, format: .currency(code: Locale.current.currencyCode ?? "USD"))")
+                Text("Your total is \(order.data.cost, format: .currency(code: Locale.current.currencyCode ?? "USD"))")
                     .font(.title)
                 
                 Button("Place Order") {
@@ -48,7 +48,7 @@ struct CheckoutView: View {
     }
     
     func placeOrder() async {
-        guard let encoded = try? JSONEncoder().encode(order) else {
+        guard let encoded = try? JSONEncoder().encode(order.data) else {
             print("Failed to encode order")
             return
         }
@@ -59,7 +59,7 @@ struct CheckoutView: View {
         request.httpMethod = "POST"
         do {
             let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
-            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            let decodedOrder = try JSONDecoder().decode(OrderModel.self, from: data)
             confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
         } catch let error {
             alertTitle = "Order Failed"
---
+++ Cupcake Corner/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Paul Hudson on 17/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var order = Order()
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.data.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    
+
+                    Stepper("Number of cakes: \(order.data.quantity)", value: $order.data.quantity, in: 3...20)
+                }
+
+                Section {
+                    Toggle("Any special requests?", isOn: $order.data.specialRequestEnabled.animation())
+
+                    if order.data.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.data.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.data.addSprinkles)
+                    }
+                }
+
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
